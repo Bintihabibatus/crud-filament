eba242d50f35bb403ac677236cc7179cbdf36cc9
Bintihabibatus <bintihabibatus@gmail.com>
2025-12-12 09:09:58 +0800

    hapus route create admin

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-// Route sementara untuk membuat user admin
-Route::get('/create-admin', function () {
-    // Ganti email dan password sesuai keinginanmu
-    $user = \App\Models\User::create([
-        'name' => 'Admin Super',
-        'email' => 'admin@contoh.com', // Ganti dengan email yang kamu mau
-        'password' => bcrypt('password123'), // Ganti dengan password yang kamu mau
-        'is_admin' => true, // Sesuaikan dengan kolom di tabel users-mu
-    ]);
-
-    return "User admin berhasil dibuat! Email: admin@contoh.com, Password: password123";
-});
